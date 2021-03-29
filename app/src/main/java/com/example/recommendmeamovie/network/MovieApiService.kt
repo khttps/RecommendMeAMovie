cef05ffa3dfbea816e99ebffde5e550b43139350
Ahmed Khattab <akhattab451@gmail.com>
2021-03-29 15:40:42 +0200

    Setup Moshi DataTransferObjects and Setup Retrofit CallAdapterFactory to Support Coroutines Response Handling

--- app/src/main/java/com/example/recommendmeamovie/network/MovieApiService.kt
+++ app/src/main/java/com/example/recommendmeamovie/network/MovieApiService.kt
@@ -1,22 +1,48 @@
 package com.example.recommendmeamovie.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://api.themoviedb.org"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .build()
 
 interface MovieApiService {
 
     @GET("3/movie/popular")
-    fun getPopularMovies(@Query("api_key") key : String) : Call<String>
+    suspend fun getPopularMovies(@Query("api_key") key : String) : String
+
+    @GET("3/discover/movie")
+    suspend fun getMovieRecommendation(
+        @Query("api_key") key : String,
+        @Query("primary_release_date.gte") releasedAfter : String,
+        @Query("primary_release_date.lte") releasedBefore : String,
+        @Query("vote_average.gte") voteGreaterThan : Int,
+        @Query("vote_average.lte") voteLessThan : Int,
+        @Query("with_genres") genre : Long
+    ) : List<Movie>
+
+    @GET("3/movie/{id}")
+    suspend fun getMovieDetails(@Path("id") id : Long, @Query("api_key") key: String) : MovieDetails
+
+    @GET("3/movie/{id}/credits")
+    suspend fun getMovieCredits(@Path("id") id : Long, @Query("api_key") key: String) : Credits
 
 }
 
